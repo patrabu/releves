aa4412de9fa0aed8ff4f487cdff23ff65307e50d
Patrick Rabu <patrick@rabu.fr>
2013-04-30 23:06:09 +0200

    Reorganisation of the sources. Change the method to get the last 30 days of log.

--- app/routes.py
+++ app/routes.py
@@ -13,38 +13,31 @@
 from datetime import datetime, timedelta
 import time
 import sqlite3
-from flask import Flask, render_template, request, jsonify, _app_ctx_stack
-
-# Configuration
-DATABASE = "releves.sqlite"
-DEBUG = True
-SECRET_KEY = 'development key'
-
-# Pattern for the datetime
-DT_FMT = "%Y-%m-%d %H:%M:%S"
-
-app = Flask(__name__)
-app.config.from_object(__name__)
-app.config.from_envvar('RELEVES_SETTINGS', silent=True)
+from flask import render_template, request, jsonify, _app_ctx_stack
+from app import app
 
 def get_db():
-    """Opens a new database connection if there is none yet for the
+    """
+    Opens a new database connection if there is none yet for the
     current application context.
     """
     top = _app_ctx_stack.top
     if not hasattr(top, 'sqlite_db'):
-        sqlite_db = sqlite3.connect(app.config['DATABASE'])
-        sqlite_db.row_factory = sqlite3.Row
-        top.sqlite_db = sqlite_db
+      #app.logger.debug("DATABASE_URI=%s", app.config['DATABASE_URI'])
+      sqlite_db = sqlite3.connect(app.config['DATABASE_URI'])
+      sqlite_db.row_factory = sqlite3.Row
+      top.sqlite_db = sqlite_db
 
     return top.sqlite_db
 
 @app.teardown_appcontext
 def close_db_connection(exception):
-    """Closes the database again at the end of the request."""
+    """
+    Closes the database again at the end of the request.
+    """
     top = _app_ctx_stack.top
     if hasattr(top, 'sqlite_db'):
-        top.sqlite_db.close()
+      top.sqlite_db.close()
 
 @app.route("/getTS")
 def getTS():
@@ -53,34 +46,6 @@ def getTS():
     """
     return jsonify(content="OK")
 
-@app.route("/getLastReleves")
-def getLastReleves():
-    """
-    Retrieve the last 10 releves
-    """
-    app.logger.debug("getLastReleves() - Begin.")
-    db = get_db()
-    cur = db.execute('''
-    select cesiLog.dtCesi 'dt', cesiLog.sensor1 's1', cesiLog.sensor2 's2',
-    cesiLog.sensor3 's3', cesiLog.appoint 'app', elecLog.elec 'elec'
-    from cesiLog
-    left outer join elecLog on cesiLog.dtCesi = elecLog.dtElec
-    order by dt desc
-    limit 10
-    ''')
-    listLog = []
-    rows = cur.fetchall()
-    for row in rows:
-        dt1 = datetime.fromtimestamp(row[0]).strftime(DT_FMT)
-        data = dict(id=int(row[0]), dt=dt1, 
-            s1=row[1], s2=row[2], s3=row[3], app=row[4], elec=row[5])
-        #app.logger.debug(data)
-        listLog.append(data)
-    app.logger.debug(listLog)
-    app.logger.debug("getLastReleves() - End.")
-    return jsonify(dict(RelevesList=listLog))
-
-
 @app.route("/get30DaysReleves")
 def get30DaysReleves():
     """
@@ -88,7 +53,7 @@ def get30DaysReleves():
     """
     app.logger.debug("get30DaysReleves() - Begin.")
     lastDay = datetime.today()
-    
+
     argLastDay = request.args.get("dt", None, type=int)
     if argLastDay != None:
         try:
@@ -96,16 +61,16 @@ def get30DaysReleves():
         except ValueError, e:
             app.logger.error("Invalid request value for get30DaysReleves() : %s", str(argLastDay))
             lastDay = datetime.today()
-    
+
     firstDay = lastDay - timedelta(days=30)
-    app.logger.debug("Search releves between %s (%d) and %s (%d).", 
-    firstDay.isoformat(), time.mktime(firstDay.timetuple()), 
+    app.logger.debug("Search releves between %s (%d) and %s (%d).",
+    firstDay.isoformat(), time.mktime(firstDay.timetuple()),
     lastDay.isoformat(), time.mktime(lastDay.timetuple()));
     db = get_db()
     cur = db.execute('''
     select cesiLog.dtCesi 'dt', cesiLog.sensor1 's1', cesiLog.sensor2 's2',
     cesiLog.sensor3 's3', cesiLog.appoint 'app', elecLog.elec 'elec'
-    from cesiLog 
+    from cesiLog
     left outer join elecLog on cesiLog.dtCesi = elecLog.dtElec
     where cesiLog.dtCesi > ? and cesiLog.dtCesi <= ?
     order by dt desc
@@ -113,8 +78,8 @@ def get30DaysReleves():
     listLog = []
     rows = cur.fetchall()
     for row in rows:
-        dt1 = datetime.fromtimestamp(row[0]).strftime(DT_FMT)
-        data = dict(id=int(row[0]), dt=dt1, 
+        dt1 = datetime.fromtimestamp(row[0]).strftime(app.config['DT_FMT'])
+        data = dict(id=int(row[0]), dt=dt1,
             s1=row[1], s2=row[2], s3=row[3], app=row[4], elec=row[5])
         #app.logger.debug(data)
         listLog.append(data)
@@ -124,10 +89,10 @@ def get30DaysReleves():
 
 @app.route('/saveReleve', methods=['POST'])
 def save():
-    """ 
-    Save e releve into the database. 
     """
-    app.logger.debug("save() - Begin.")
+    Save e releve into the database.
+    """
+    #app.logger.debug("save() - Begin.")
     errors = []
     db = get_db()
 
@@ -145,10 +110,10 @@ def save():
         errors.append(dict(field="date", message="The date is mandatory"))
     else:
         try:
-            dtReleve = datetime.strptime(dt, DT_FMT)
-            if dtReleve > datetime.datetime.now():
+            dtReleve = datetime.strptime(dt, app.config['DT_FMT'])
+            if dtReleve > datetime.now():
                 errors.append(dict(field="date", message="Date is in future."))
-        except valueError:
+        except ValueError:
             errors.append(dict(field="date", message="The date is incorrect"))
 
     if s2 > s3:
@@ -163,48 +128,45 @@ def save():
         row = cur.fetchone()
         if row :
             # Get the electricity index
-            app.logger.debug("save() - Last row = %s", row[0])
+            #app.logger.debug("save() - Last row = %s", row[0])
             cur = db.execute('select elec from elecLog where dtElec = :dtElec',
                 {"dtElec": row[0]})
             row2 = cur.fetchone()
             if row2 :
-                app.logger.debug("save() - Last electricity index = %s", row2[0])
+                #app.logger.debug("save() - Last electricity index = %s", row2[0])
                 if elec < row2[0]:
                     app.logger.debug("save() - error elec: %d < %d", elec, row2[0])
                     errors.append(dict(field="elec", message="Electricity index is less than previous one {}.".format(row2[0])))
 
     if len(errors) == 0:
         cur = db.cursor()
-        app.logger.debug("save() - Id=%s", id)
-        if id <= 0:
-            try:
-                app.logger.debug("save() - insert cesiLog")
+        #app.logger.debug("save() - Id=%s", id)
+        try:
+            if id <= 0:
+                #app.logger.debug("save() - insert cesiLog")
                 cur.execute('insert into cesiLog values (?, ?, ?, ?, ?)',
                     (tsReleve, s1, s2, s3, appoint))
                 if elec != None:
-                    app.logger.debug("save() - insert elecLog")
+                    #app.logger.debug("save() - insert elecLog")
                     cur.execute('insert into elecLog values (?, ?)',
                         (tsReleve, elec))
-            except sqlite3.Error as e:
-                app.logger.error("save() - Insert error=", e.args[0])
-        else:
-            try:
+            else:
                 if s1 != None or s2 != None or s3 != None:
-                    app.logger.debug("save() - update cesiLog")
+                    #app.logger.debug("save() - update cesiLog")
                     cur.execute('''update cesiLog
                         set dtCesi=?, sensor1=?, sensor2=?, sensor3=?, appoint=?
                         where dtCesi = ?''',
                         (tsReleve, s1, s2, s3, appoint, id))
                 if elec != None:
-                    app.logger.debug("save() - update elecLog")
+                    #app.logger.debug("save() - update elecLog")
                     cur.execute('''update elecLog
                         set dtElec=?, elec=? where dtElec = ?''',
                         (tsReleve, elec, id))
-            except sqlite3.Error as e:
-                app.logger.error("save() - Update error=", e.args[0])
+        except sqlite3.Error as e:
+            app.logger.error("save() - DB error=", e.args[0])
         cur.close
         db.commit()
-        app.logger.debug("Id=%s", id)
+        #app.logger.debug("Id=%s", id)
         return jsonify(content={"returnCode": "OK", "id": tsReleve})
     else:
         for err in errors:
@@ -223,6 +185,3 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
-
-if __name__ == '__main__':
-  app.run('192.168.0.100', debug=True)
