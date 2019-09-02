32254dd7440ba886f635a95ea9aefc78934a83df
Patrick Rabu <patrick@rabu.fr>
2019-09-02 19:18:27 +0200

    Change server address ip.

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 #!venv/bin/python
+
 # encoding: utf-8
 
 """
@@ -12,4 +13,4 @@
 """
 
 from app import app
-app.run(host='0.0.0.0', port=5000, debug=True)
+app.run(host='127.0.0.1', port=5000, debug=True)
